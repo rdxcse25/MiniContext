7b0305d142c51a8ac0b9a2bb4d3012f0734829f5
Rituraj Debnath <princerituraj20@gmail.com>
2024-10-21 09:35:40 +0530

    first commit

---
+++ src/context/UserContext.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const UserContext = React.createContext()
+
+export default UserContext;
+
+//Global UserContext is a provider...provides data
+//eg.
+{/* <UserContext>
+    <Login/>
+    <Card>
+        <Data/>
+    </Card>
+</UserContext> */}
+
